fd49abf1e53cc3eaebe492644f4c0d92b2199128
Subash <subashmurugan2021@gmail.com>
2025-02-03 15:23:43 +0530

    add no data image in record component

--- frontend/src/components/record/Record.jsx
+++ frontend/src/components/record/Record.jsx
@@ -60,69 +60,75 @@ const Record = ({isAddIcon}) => {
     <div id="body">
       <div className="header3">பதிவுகள்</div>
       <hr className="divider" />
-
+  
       <div className="record-list">
-        {records?.filter(record => record.createdAt.split("T")[0] === searchDate)
-          .map((record, key) => (
-            <div 
-              className={`detail-item ${activeRecord === key ? "active" : ""}`} 
-              key={key} 
-              onClick={() => setActiveRecord(activeRecord === key ? null : key)}
-            >
-              {activeRecord === key && (
-                <div className="three-dots" onClick={(e) => { 
-                  e.stopPropagation();
-                  setShowModal(showModal === key ? null : key);
-                }}>
-                  <BsThreeDotsVertical size={18} />
-                </div>
-              )}
-
-              <div className="image-container">
-                <img src="https://static.thenounproject.com/png/1123247-200.png" alt="Attachment" />
-              </div>
-
-              <div className="detail-content">
-                <p className="details">{record.reason}</p>
+        {records?.filter(record => record.createdAt.split("T")[0] === searchDate).length === 0 ? (
+          // Show this image when there are no records
+          <div className="no-records">
+            <img src="no-data-img.png" className='no-data-img' alt="No Records Found" />
+            
+          </div>
+        ) : (
+          records?.filter(record => record.createdAt.split("T")[0] === searchDate)
+            .map((record, key) => (
+              <div 
+                className={`detail-item ${activeRecord === key ? "active" : ""}`} 
+                key={key} 
+                onClick={() => setActiveRecord(activeRecord === key ? null : key)}
+              >
                 {activeRecord === key && (
-                  <div>
-                    {record.person && (
-                      <p className="contact">{`${record.person} | ${record.mobile || ""}`}</p>
-                    )}
-                    <p className="date-time">{`தேதி: ${record.createdAt.split("T")[0]} | ${new Date(record.createdAt).toLocaleTimeString()}`}</p>
-                    <div className="file-info">
-                      {record.image && <img onClick={() => openModal("image", record.image)} src={record.image} alt="File" />}
-                      {record.image || record.audio ? <p onClick={() => openModal("image", record.image)}>Attachments</p>:"" }
-                      
+                  <div className="three-dots" onClick={(e) => { 
+                    e.stopPropagation();
+                    setShowModal(showModal === key ? null : key);
+                  }}>
+                    <BsThreeDotsVertical size={18} />
+                  </div>
+                )}
+  
+                <div className="image-container">
+                  <img src="https://static.thenounproject.com/png/1123247-200.png" alt="Attachment" />
+                </div>
+  
+                <div className="detail-content">
+                  <p className="details">{record.reason}</p>
+                  {activeRecord === key && (
+                    <div>
+                      {record.person && (
+                        <p className="contact">{`${record.person} | ${record.mobile || ""}`}</p>
+                      )}
+                      <p className="date-time">{`தேதி: ${record.createdAt.split("T")[0]} | ${new Date(record.createdAt).toLocaleTimeString()}`}</p>
+                      <div className="file-info">
+                        {record.image && <img onClick={() => openModal("image", record.image)} src={record.image} alt="File" />}
+                        {record.image || record.audio ? <p onClick={() => openModal("image", record.image)}>Attachments</p> : ""}
+                      </div>
+                      {record.audio &&
+                      <div className="speaker-icon">
+                        <BsFillVolumeUpFill size={16} onClick={() => openModal("audio", record.audio)} color="#A0A0A0" />
+                      </div>}
                     </div>
-                    {record.audio &&
-                    <div className="speaker-icon">
-                    <BsFillVolumeUpFill size={16} onClick={() => openModal("audio", record.audio)} color="#A0A0A0" />
-                  </div>}
+                  )}
+                </div>
+  
+                <div className={record.catagory === "expense" ? "amount expense" : "amount"}>
+                  ₹ {record.amount}
+                </div>
+  
+                {showModal === key && (
+                  <div className="modal">
+                    <div className="modal-content">
+                      <button onClick={() => handleDelete(record._id)}>Delete</button>
+                    </div>
                   </div>
                 )}
               </div>
-
-              
-
-              <div className={record.catagory === "expense" ? "amount expense" : "amount"}>
-                ₹ {record.amount}
-              </div>
-
-              {showModal === key && (
-                <div className="modal">
-                  <div className="modal-content">
-                    <button onClick={() => handleDelete(record._id)}>Delete</button>
-                  </div>
-                </div>
-              )}
-            </div>
-          ))}
+            ))
+        )}
       </div>
-     {isAddIcon && <button className="add-button" onClick={() => setIsOpen(true)}>+</button>}
+  
+      {isAddIcon && <button className="add-button" onClick={() => setIsOpen(true)}>+</button>}
       
       {isOpen && <AddDetails />}
-
+  
       {showPopupModal && (
         <PopupModal
           isImage={isImage}
@@ -133,6 +139,7 @@ const Record = ({isAddIcon}) => {
       )}
     </div>
   );
+  
 };
 
 export default Record;
